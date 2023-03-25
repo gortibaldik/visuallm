98c341d71bd5e55b27761bc693e46803f4cb0624
gortibaldik <ferotre@gmail.com>
2023-03-25 20:35:20 +0100

    Fix frontend

--- client_llm_generation/vite.config.ts
+++ client_llm_generation/vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: path.join("..", "server_llm_generation", "dist")
+    outDir: path.join("..", "llm_generation_server", "dist")
   }
 })
