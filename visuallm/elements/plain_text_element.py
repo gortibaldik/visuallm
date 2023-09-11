e1ac9900822886cbc0d750732095d200af06c8ff
gortibaldik <ferotre@gmail.com>
2023-09-11 11:01:40 +0200

    Add Heading Element

--- visuallm/elements/plain_text_element.py
+++ visuallm/elements/plain_text_element.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from .element_base import ElementBase
 
 
@@ -15,17 +17,24 @@ class PlainTextElement(ElementBase):
         self.heading_level = heading_level
 
     @property
-    def content(self):
+    def content(self) -> str:
         return self._content
 
     @content.setter
-    def content(self, value):
+    def content(self, value: str):
         self.changed = True
         self._content = value
 
-    def construct_element_configuration(self):
+    def construct_element_configuration(self) -> Dict[str, Any]:
         return dict(
             value=self.content,
             heading=self.is_heading,
             heading_level=self.heading_level,
+        )
+
+
+class HeadingElement(PlainTextElement):
+    def __init__(self, content: str = "", heading_level=3, name="heading"):
+        super().__init__(
+            content, is_heading=True, heading_level=heading_level, name=name
         )
