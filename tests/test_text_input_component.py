2e43f81304633860077280ca8989bdfadca4aff2
Evgeniya Knyazeva <knjazevazena@gmail.com>
2023-09-12 16:00:03 +0200

    tests are running on own instance of the app

--- tests/test_text_input_component.py
+++ tests/test_text_input_component.py
@@ -1,22 +1,33 @@
 import unittest
+import urllib3
+from examples_py.app import app
+from flask import Flask
+from flask_testing import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-def getHeadlessFirefox():
-    opts = FirefoxOptions()
-    opts.add_argument('--headless')
-    return webdriver.Firefox(options=opts)
+class FlaskSeleniumFirefoxTestBase(LiveServerTestCase):
 
-class TextInputComponent_InsertTextInTextarea(unittest.TestCase):
+    def create_app(self):
+        app.config['LIVESERVER_PORT'] = 5000
+        # Default timeout is 5 seconds
+        app.config['LIVESERVER_TIMEOUT'] = 10
+        return app
 
     def setUp(self):
         opts = FirefoxOptions()
         opts.add_argument('--headless')
         self.driver = webdriver.Firefox(options=opts)
         self.driver.implicitly_wait(2)
-        self.driver.get('http://127.0.0.1:5000/index.html#/text_input_component')
+        self.driver.get(self.get_server_url() + "/index.html#/text_input_component")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+class TextInputComponent_InsertTextInTextarea(FlaskSeleniumFirefoxTestBase):
 
     def test_insert_text_in_textarea(self):
         driver = self.driver
