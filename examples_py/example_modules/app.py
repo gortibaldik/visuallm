314964d27d50af70f7bb72955ca7548b00642ed6
gortibaldik <ferotre@gmail.com>
2023-09-19 12:37:13 +0200

    Restructure tests, other impl of webdriver and flask test server

--- examples_py/example_modules/app.py
+++ examples_py/example_modules/app.py
@@ -1,3 +1,5 @@
+import flask
+
 from visuallm.server import Server
 
 from .components.bar_chart_component_advanced import BarChartComponentAdvanced
@@ -7,16 +9,23 @@ from .components.table_component import TableComponent
 from .components.text_input_component import TextInputComponent
 from .components.two_tables_component import TwoTablesComponent
 
-flask_app = Server(
-    __name__,
-    [
-        BarChartComponentAdvanced(),
-        BarChartComponentSimple(),
-        BarChartComponentSimple(long_contexts=True, title="Long Contexts BarChart"),
-        TableComponent(),
-        TwoTablesComponent(),
-        SelectorComponent(),
-        TextInputComponent(),
-    ],
-)
-app = flask_app.app
+
+def create_app() -> flask.Flask:
+    flask_app = Server(
+        __name__,
+        [
+            BarChartComponentAdvanced(),
+            BarChartComponentSimple(),
+            BarChartComponentSimple(long_contexts=True, title="Long Contexts BarChart"),
+            TableComponent(),
+            TwoTablesComponent(),
+            SelectorComponent(),
+            TextInputComponent(),
+        ],
+    )
+    app = flask_app.app
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
