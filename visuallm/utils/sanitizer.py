0db6159cd88f1002017a5011dc997b4efe9edfba
gortibaldik <ferotre@gmail.com>
2023-11-26 18:49:41 +0100

    ordering of decorators

--- visuallm/utils/sanitizer.py
+++ visuallm/utils/sanitizer.py
@@ -31,25 +31,25 @@ class Sanitizer:
         value = value.replace("</code>", "")
         return "<" not in value and ">" not in value
 
-    @staticmethod
     @singledispatch
+    @staticmethod
     def sanitize(value):
         raise NotImplementedError()
 
-    @staticmethod
     @sanitize.register
+    @staticmethod
     def sanitize_int(value: int):
         return Sanitizer.sanitize_str(str(value))
 
-    @staticmethod
     @sanitize.register
+    @staticmethod
     def sanitize_str(value: str):
         value = Sanitizer.convert_brackets(value)
         value = Sanitizer.convert_newline_to_br_tags(value)
         value = Sanitizer.convert_backticks(value)
         return value
 
-    @staticmethod
     @sanitize.register
+    @staticmethod
     def sanitize_list_str(values: list) -> list:
         return [Sanitizer.sanitize(value) for value in values]
